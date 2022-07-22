18f4f410aaa3aa18cad5c4a9ba8cee6a9bad831d
Guilherme <guiggff@gmail.com>
2022-07-22 18:10:18 -0300

    :bugfix: Bug do babel

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports =  {
-    // getDefaultConfig(__dirname);
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-}
+module.exports = {
+  // getDefaultConfig(__dirname);
+  assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+};
